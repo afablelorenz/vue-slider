9df2cf7baee74f3c81f66ffc251de27a6322bf02
afablelorenz <afablelorenz@gmail.com>
2024-05-29 19:08:16 +0200

    update script

--- js/script.js
+++ js/script.js
@@ -22,4 +22,70 @@ const slides = [
     }
 ];
 
+
+const {createApp} = Vue 
+
+
+createApp({
+    data(){
+        return{
+            index : 0,
+            arr: [
+                {
+                    image: 'img/01.webp',
+                        title: 'Marvel\'s Spiderman Miles Morale',
+                        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    }, {
+                        image: 'img/02.webp',
+                        title: 'Ratchet & Clank: Rift Apart',
+                        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    }, {
+                        image: 'img/03.webp',
+                        title: 'Fortnite',
+                        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    }, {
+                        image: 'img/04.webp',
+                        title: 'Stray',
+                        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    }, {
+                        image: 'img/05.webp',
+                        title: "Marvel's Avengers",
+                        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    }
+                ],
+        }
+    },
+    methods: {
+        getTitle: function(){
+            if(this.index>=0 || this.index < 4){
+                return this.arr[this.index].title;
+            }else{
+                return this.arr[0].title;
+            }
+            
+        },
+        getText: function(){
+            if(this.index>=0 || this.index < 4){
+                return this.arr[this.index].text;
+            }else{
+                return this.arr[0].text;
+            };
+        },
+        getImage: function(){
+            if(this.index>=0 || this.index < 4){
+                return this.arr[this.index].image;
+            }else{
+                return this.arr[0].image;
+            };
+        },
+        nextItem: function(){
+            if(this.index >= 0 || this.index < 4){
+                this.index = this.index+1;
+            }else{
+                this.index = 0;
+            }
+        }        
+    }
+}).mount('#app');
+
 console.log(slides);
